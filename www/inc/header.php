f6c249eb146a0ecf146f898f632ebb89692a029c
joro <joro@liquidsoft.eu>
2018-02-01 16:27:24 -0500

    change header

---
+++ www/inc/header.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  </head>
+  <body>
+    <h1>
+      Thundermeter Sender
+    </h1>
+   
+    <div class="container-fluid">
+      <ul class="nav">
+        <li class="nav-item">
+          <a class="nav-link active" href="#">Campaigns</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Recipients</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Setup</a>
+        </li>
+      </ul>
+    </div>
