a50d04d9d369f998947fd0c79ca37d7b839de60b
EC2 Default User <ec2-user@ip-172-31-30-171.eu-west-1.compute.internal>
2018-04-16 16:36:27 +0000

    add resourcces

--- www/inc/header.php
+++ www/inc/header.php
@@ -18,7 +18,7 @@
           <a class="nav-link" href="#">Recipients</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Setup</a>
+          <a class="nav-link" href="#">Users</a>
         </li>
       </ul>
     </div>
